ce1acadedcbd707d97e0e30777adff9c2105df95
Hiro.Ogata <ogata.hiro@gmail.com>
2024-03-16 09:24:17 +0900

    astroのoutDirを再調整

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   base: '/notes',
-  outDir: '../astro/notesa/',
+  outDir: '../notes/',
 });
