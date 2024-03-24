562e0ec95b835d972853224ab7b10ed52ae6b884
Hiro.Ogata <ogata.hiro@gmail.com>
2024-03-24 21:50:19 +0900

    アップデート

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,23 +3,23 @@ import expressiveCode from "astro-expressive-code";
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   base: "/astro/notes",
   outDir: "./notes/",
   build: {
-    assets: "assets",
+    assets: "assets"
   },
-  integrations: [
-    expressiveCode({
-      plugins: [pluginLineNumbers()],
-      defaultProps: {
-        showLineNumbers: false,
-      },
-    }),
-  ],
+  integrations: [expressiveCode({
+    plugins: [pluginLineNumbers()],
+    defaultProps: {
+      showLineNumbers: false
+    }
+  }), tailwind()],
   markdown: {
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
-  },
+    rehypePlugins: [rehypeKatex]
+  }
 });
